9367253b381127b3604c0e9604113b8c8d877508
Jan Melcher <info@yogularm.de>
2014-04-02 01:22:59 +0200

    Implemented TaskContext (performs one task at a time)

---
+++ spec/taskContextSpec.js
@@ -0,0 +1,75 @@
+var TaskContext = require('../src/task-context.js');
+var Task = require('../src/task.js');
+
+describe("TaskContext", function() {
+	it("should be idle after creation", function() {
+		var context = new TaskContext();
+		expect(context.isBusy).toBe(false);
+	});
+	
+	it("should run task directly if not busy", function(done) {
+		var context = new TaskContext();
+		var task = new Task();
+		var isPerformed = false;
+		var isRunEmitted = false;
+		task.perform = function(resolve) { isPerformed = true; resolve(); };
+		context.on("run", function() {
+			isRunEmitted = true;
+		});
+		context.on("done", function() {
+			expect(isPerformed).toBe(true);
+			expect(isRunEmitted).toBe(true);
+			done();
+		});
+		
+		context.schedule(task);
+	});
+	
+	it("should run second task when first task is done", function(done) {
+		var context = new TaskContext();
+		var task1 = new Task();
+		var task2 = new Task();
+		var resolveTask1;;
+		var task1Performed = false;
+		var task2Performed = false;
+		var runEmitTimes = 0;
+		var doneEmitTimes = 1;
+		task1.perform = function(resolve) { task1Performed = true; resolveTask1 = resolve; };
+		task2.perform = function(resolve) { task2Performed = true; resolve(); };
+		context.on("run", function() { runEmitTimes++; });
+		context.on("done", function() { doneEmitTimes++; });
+		
+		context.schedule(task1);
+		context.schedule(task2);
+		
+		expect(task1Performed).toBe(true);
+		expect(task2Performed).toBe(false);
+		
+		resolveTask1();
+		
+		task2.then(function() {
+			expect(task2Performed).toBe(true);
+			expect(runEmitTimes).toEqual(2);
+			expect(doneEmitTimes).toEqual(2);
+			done();
+		});
+	});
+	
+	it("should emit the status event", function(done) {
+		var context = new TaskContext();
+		var task = new Task();
+		task.perform = function(resolve) { resolve(); };
+		var statusEmitIndex = 0;
+		context.on("status", function() {
+			if (statusEmitIndex == 0) {
+				expect(context.isBusy).toBe(true);
+				statusEmitIndex++;
+			} else if (statusEmitIndex == 1) {
+				expect(context.isBusy).toBe(false);
+				done();
+			}
+		});
+		
+		context.schedule(task);
+	});
+});
---
+++ src/task-context.js
@@ -0,0 +1,46 @@
+var EventEmitter = require('events').EventEmitter;
+
+function TaskContext() {
+	EventEmitter.call(this);
+	this.isBusy = false;
+	this.currentTask = null;
+	this._queue = [];
+}
+
+TaskContext.prototype = Object.create(EventEmitter.prototype);
+
+TaskContext.prototype.schedule = function(task) {
+	if (this.isBusy) {
+		this._queue.push(task);
+	} else {
+		this.isBusy = true;
+		this.emit('status');
+		this._runTask(task);
+	}
+};
+
+TaskContext.prototype._runNextTask = function() {
+	if (this._queue.length == 0) {
+		this.currentTask = null;
+		this.isBusy = false;
+		this.emit('status');
+	} else {
+		var task = this._queue.pop();
+		this._runTask(task);
+	}
+};
+
+TaskContext.prototype._runTask = function(task) {
+	this.currentTask = task;
+	this.emit('run', task);
+	task.start();
+	task.then(function(result) {
+		this.emit('done', task, result);
+		this._runNextTask();
+	}.bind(this), function(error) {
+		this.emit('fail', task, error);
+		this._runNextTask();
+	}.bind(this));
+};
+
+module.exports = TaskContext;
