1ae8c2ed9646ad20a13f9a7ff516d216a7ff4f10
Jan Melcher <info@yogularm.de>
2014-04-02 20:17:28 +0200

    Implemented ShellTask to execute any shell command

---
+++ spec/shellTaskSpec.js
@@ -0,0 +1,41 @@
+var ShellTask = require('../src/tasks/shell.js');
+
+describe("ShellTask", function() {
+	it("returns the stdout", function(done) {
+		var task = new ShellTask('echo "hi"');
+		task.start();
+		task.then(function(stdout) {
+			expect(stdout).toEqual("hi\n");
+			done();
+		});
+	});
+
+	it("specifies command in its name", function() {
+		var task = new ShellTask("ls");
+		expect(task.name).toBe("exec ls");
+	});
+	
+	it("logs the command, stdout and stderr", function(done) {
+		var task = new ShellTask('echo "bad" >&2 ; echo "good"');
+		var log = [];
+		task.on("log", function(message) {
+			log.push(message);
+		});
+		
+		task.start();
+		task.then(function() {
+			expect(log).toEqual(["stdout: good\n",
+			                     "stderr: bad\n"]);
+			done();
+		});
+	});
+	
+	it("rejects if exit code is not zero", function(done) {
+		var task = new ShellTask('exit 13');
+		task.start();
+		task.catch(function(error) {
+			expect(error.code).toEqual(13);
+			done();
+		});
+	});
+});
---
+++ src/tasks/shell.js
@@ -0,0 +1,30 @@
+var Task = require('../task.js');
+var exec = require('child_process').exec;
+
+function ShellTask(command, cwd) {
+	Task.call(this);
+	this.name = "exec " + command + (cwd ? " (in " + cwd + ")" : '');
+	this.command = command;
+	this.cwd = cwd;
+}
+
+ShellTask.prototype = Object.create(Task.prototype);
+
+ShellTask.prototype.perform = function(resolve, reject) {
+	var options = {};
+	if (this.cwd)
+		options.cwd = this.cwd;
+	exec(this.command, function(error, stdout, stderr) {
+		if (stdout)
+			this.log("stdout: " + stdout);
+		if (stderr)
+			this.log("stderr: " + stderr);
+		if (error !== null) {
+			reject(error);
+		} else {
+			resolve(stdout);
+		}
+	}.bind(this));
+};
+
+module.exports = ShellTask;
