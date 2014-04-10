87408f973b44715e6cc87fc5ea6bd5ee299577cb
Jan Melcher <info@yogularm.de>
2014-04-10 11:09:45 +0200

    fixed default site location for server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var server = require('./src/server.js');
 var path = require('path');
 
-server.start(process.env.PORT || 8888, path.resolve(__dirname, 'spec/resources/site-collection'));
+server.start(process.env.PORT || 8888, path.resolve(__dirname, 'test/server-unit/resources/site-collection'));
