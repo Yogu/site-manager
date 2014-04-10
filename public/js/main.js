01e6cbdbbd22e1d15838999235449126f357f3c1
Jan Melcher <info@yogularm.de>
2014-04-10 10:18:07 +0200

    fixed angular.js bootstrap

--- public/js/main.js
+++ public/js/main.js
@@ -33,7 +33,7 @@ require( [
 		var $html = angular.element(document.getElementsByTagName('html')[0]);
 		
 		angular.element().ready(function() {
-		if (angular.resumeBootsrap)
+		if (angular.resumeBootstrap)
 			angular.resumeBootstrap([app['name']]);
 	});
 });
