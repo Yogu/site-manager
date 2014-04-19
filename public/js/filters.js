ff71cf0376627fdbe585cc6a8bc770ac9a9101dd
Jan Melcher <info@yogularm.de>
2014-04-19 02:16:18 +0200

    Changed yellow to orange in web console

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,4 +1,6 @@
 define(['angular', 'ansi2html'], function(angular, ansi2html) {
+	ansi2html.table[33] = { color: 'rgb(255, 133, 0)' };
+	
 	angular.module('myApp.filters', ['ngSanitize'])
 	.filter('ansi2html', ['$sce', function($sce) {
 		return function(ansi) {
