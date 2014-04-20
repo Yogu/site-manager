1cc599fb34a1fb86fbe118fef5940a2562fb3bc8
Jan Melcher <info@yogularm.de>
2014-04-20 14:16:48 +0200

    Moved test server to test/ directory and added main index.js
    the main index.js exports objects that should be used by site-manager
    consuments

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var server = require('./src/server.js');
-var path = require('path');
-var resources = require('./test/utils/resources.js');
-
-resources.use(function(path) {
-	server.start(process.env.PORT || 8888, path + '/site-collection');
-});
+exports.Server = require('./src/server.js');
+exports.hooks = require('./src/hooks.js');
+exports.Task = require('./src/task.js');
---
+++ test/index.js
@@ -0,0 +1,7 @@
+var server = require('../src/server.js');
+var path = require('path');
+var resources = require('./utils/resources.js');
+
+resources.use(function(path) {
+	server.start(process.env.PORT || 8888, path + '/site-collection');
+});
